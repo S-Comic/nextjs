73785908861ea6e584945aeb4cfe8a3c02fbe740
Samuel Jule <32823294+S-Comic@users.noreply.github.com>
2021-04-27 22:02:11 +0100

    Removed references to Layouts

--- pages/worlds/[id].js
+++ pages/worlds/[id].js
@@ -3,18 +3,17 @@ import Head from 'next/head'
 import styles from '../../styles/worlds.module.css'
 import posts from '../static/worldData.json'
 import { useRouter } from 'next/router'
-import Layout from "../../components/layout";
-import Header from "../../components/header";
 
-const Post = props => <layout>
-    <Header />
+const Post = props => {
+  return (
     <main className={styles.worlds}>
       <section className={styles.heroBanner}>
         <h1 className={styles.heroTitle}> Welcome to {props.post.title}</h1>
         <p> {props.post.subtitle} </p>
       </section>
     </main>
-</layout>
+  )
+}
 Post.getInitialProps = ({ query }) => {
   return {
     post: posts[query.id]
