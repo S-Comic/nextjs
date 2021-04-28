6ae40c5b34a821b3d686bfb2550bb58ae245166e
Samuel Jule <32823294+S-Comic@users.noreply.github.com>
2021-04-28 21:55:26 +0100

    Added header and trying out material UI

--- components/header.js
+++ components/header.js
@@ -1,15 +1,15 @@
-const headerStyle = {
-  backgroundColor: "blue",
-  color: "white",
-  position: "absolute",
-  width: "100%",
-  height: "50px"
-};
-
 const Header = () => (
-  <div className="Header" style={headerStyle}>
-    HEADER
-  </div>
+  <header className="pageHeader">
+    <div className="navBrand">
+      <img src="../images/dndgreen.png" alt="Vercel Logo" />
+    </div>
+
+    <nav className="navbar">
+      <p>Home</p>
+      <p>About</p>
+      <p>New page</p>
+    </nav>
+  </header>
 );
 
 export default Header;
