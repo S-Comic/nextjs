df7ac13995bc4a5402730c9e6b8580b43b47fed8
S-Comic <samjule@outlook.com>
2021-04-28 15:31:35 +0100

    Testing JS importing, added chart.js

--- pages/worlds/[id].js
+++ pages/worlds/[id].js
@@ -3,15 +3,19 @@ import Head from 'next/head'
 import styles from '../../styles/worlds.module.css'
 import posts from '../static/worldData.json'
 import { useRouter } from 'next/router'
+import Header from '../../components/header.js'
 
 const Post = props => {
   return (
+    <body>
+    <Header />
     <main className={styles.worlds}>
       <section className={styles.heroBanner}>
         <h1 className={styles.heroTitle}> Welcome to {props.post.title}</h1>
         <p> {props.post.subtitle} </p>
       </section>
     </main>
+    </body>
   )
 }
 Post.getInitialProps = ({ query }) => {
